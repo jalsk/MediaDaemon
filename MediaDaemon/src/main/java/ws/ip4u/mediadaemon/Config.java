098d280ca37cd5add2fa5c361db1f65881497f88
Ryan Rapp <jalsk957@gmail.com>
2011-10-08 22:34:46 -0600

    Fixed more Sonar issues.

--- MediaDaemon/src/main/java/ws/ip4u/mediadaemon/Config.java
+++ MediaDaemon/src/main/java/ws/ip4u/mediadaemon/Config.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Serializable;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
